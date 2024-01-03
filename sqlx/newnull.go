122a461efb4945bad9bb1a64991d4d09b6b6c23b
wureny <wurenyu19@gmail.com>
2024-01-03 22:00:37 +0800

    提供了newnull系列方法

---
+++ sqlx/newnull.go
@@ -0,0 +1,32 @@
+package sqlx
+
+import (
+	"database/sql"
+	"time"
+)
+
+//这一个系列的方法，会在数据为零值时，将valid设置为false；否则设置为true；
+
+func NewNullString(val string) sql.NullString {
+	return sql.NullString{String: val, Valid: val != ""}
+}
+
+func NewNullInt64(val int64) sql.NullInt64 {
+	return sql.NullInt64{Int64: val, Valid: val != 0}
+}
+
+func NewNullFloat64(val float64) sql.NullFloat64 {
+	return sql.NullFloat64{Float64: val, Valid: val != 0}
+}
+
+func NewNullBool(val bool) sql.NullBool {
+	return sql.NullBool{Bool: val, Valid: val}
+}
+
+func NewNullTime(val time.Time) sql.NullTime {
+	return sql.NullTime{Time: val, Valid: !val.IsZero()}
+}
+
+func NewNullBytes(val []byte) sql.NullString {
+	return sql.NullString{String: string(val), Valid: len(val) > 0}
+}
