4d2da03e940012997003f707a22602164064efc0
wureny <wurenyu19@gmail.com>
2024-02-17 09:19:36 +0800

    feat: add new method for Pair

--- pair/pair.go
+++ pair/pair.go
@@ -8,3 +8,32 @@ type Pair[K any, V any] struct {
 func NewPair[K any, V any](key K, value V) *Pair[K, V] {
 	return &Pair[K, V]{Key: key, Value: value}
 }
+
+func (p *Pair[K, V]) GetKey() K {
+	return p.Key
+}
+
+func (p *Pair[K, V]) GetValue() V {
+	return p.Value
+}
+
+func (p *Pair[K, V]) SetKey(key K) {
+	p.Key = key
+}
+
+func (p *Pair[K, V]) SetValue(value V) {
+	p.Value = value
+}
+
+func (p *Pair[K, V]) Set(key K, value V) {
+	p.Key = key
+	p.Value = value
+}
+
+func (p *Pair[K, V]) Clone() *Pair[K, V] {
+	return &Pair[K, V]{Key: p.Key, Value: p.Value}
+}
+
+func (p *Pair[K, V]) Split(pair *Pair[K, V]) (K, V) {
+	return pair.Key, pair.Value
+}
